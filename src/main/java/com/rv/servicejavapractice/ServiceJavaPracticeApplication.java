5c32e5ca283bde361811538135085469810bb837
rvthummar3 <rvthummar3@gmail.com>
2022-04-10 09:15:47 +0530

    Initial commit

---
+++ src/main/java/com/rv/servicejavapractice/ServiceJavaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.rv.servicejavapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceJavaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceJavaPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rv/servicejavapractice/ServiceJavaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rv.servicejavapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceJavaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
